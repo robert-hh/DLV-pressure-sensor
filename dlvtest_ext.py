25814958320109742d7e1c211177b1c9ea4367ac
robert <robert@hammelrath.com>
2020-09-23 20:54:21 +0200

    dlvtest*.py: Add dlvtest_ext.py
    Remove all commented settings for other boards from dlvtest_ext.py

--- dlvtest_ext.py
+++ dlvtest_ext.py
@@ -25,9 +25,40 @@ import time
 import dlv_ps_ext as dlv_ps
 
 from machine import I2C
-i2c=I2C(1, freq=400000)  # XBEE 3 Hardware I2C
+i2c=I2C(-1, scl=Pin(25), sda=Pin(26), freq=400000) ## ESP32 Soft
+
+# i2c=I2C(scl=Pin(5), sda=Pin(4), freq=400000) ## ESP8266 Soft
+
+# i2c=I2C(0, scl=Pin(Pin.PB_13), sda=Pin(Pin.PB_14), freq=400000) ## W600 Hard
+# i2c=I2C(-1, scl=Pin(Pin.PB_13), sda=Pin(Pin.PB_14), freq=400000) ## W600 Hard
+
+# i2c=I2C(scl="X1", sda="X2", freq=400000) ## PyBoard Soft
+# i2c=I2C("X", freq=400000) ## PyBoard Hard
+# Pin("EN_3V3")(1) # enable 3.3V Output on Pyboard D
+
+# i2c=I2C(0, pins=('P9','P10'), baudrate=400000) # Pycom Hard I2C
+# i2c=I2C(2, pins=('P9','P10'), baudrate=400000) # Pycom Soft I2C
+
+# import busio    # Circuitpython
+# import board    # Circuitpython
+# i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)    # Circuitpython Hard I2C
+# i2c.try_lock()    # Circuitpython
+
+# i2c=I2C(1, freq=400000) # XBEE 3 Hard I2C
 
 dlv=dlv_ps.DLV_I2C(i2c, model="030g", offset=1638)
+
+# from machine import SPI
+# spi =SPI(-1, baudrate=10000, sck=Pin(12, Pin.OUT), miso=Pin(13, Pin.IN), mosi=Pin(4), polarity=0, phase=0)
+# dlv=dlv_ps.DLV_SPI((spi, Pin(27, Pin.OUT),), model="015D") # esp32 Soft SPI
+
+# dlv=dlv_ps.DLV_SPI((Pin(25, Pin.OUT), Pin(26, Pin.IN), Pin(27, Pin.OUT),),
+#                    model="015D") # esp32 Pin BB
+
+# dlv=dlv_ps.DLV_SPI((Pin("D1", Pin.OUT), Pin("D3", PIN.IN), Pin("D11", Pin.OUT),),
+#                    model="015D") # XBEE Pin BB
+# XBEE Pin BB is practically useless. Freq 200 Hz, total time 160ms.
+# Fastest Pin toggle time 2.5 ms. Other ports are in the range 1-4 µs.
 
 def run(readall=True):
 
